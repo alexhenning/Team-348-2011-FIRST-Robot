7f3fccf42e040d018ffa84f3bc2c9468eb087e49
Alex Henning <elcron@gmail.com>
2011-03-16 21:03:08 -0400

    Fixing encoder issue...

--- src/org/usfirst/frc348/Arm.java
+++ src/org/usfirst/frc348/Arm.java
@@ -17,7 +17,7 @@ public class Arm implements PIDSource {
     protected boolean magicMode = true, placing = false;
 
     // Position to move the arm to based off of the controller
-    protected static double positions[] = {-1, 1.87, 1.680, 0.915, 0.700, -2};
+    protected static double positions[] = {-1, 1.87, 1.680, 0.915, 0.700, 0};
 
     public Arm(int jagID, int servoPort, int limitPort, int bannerPort) throws CANTimeoutException {
     	jag = Utils.getJaguar(jagID);
@@ -49,10 +49,10 @@ public class Arm implements PIDSource {
 
     public void periodic() {
 	if (limit.get()) {
-	    pid.setOutputRange(-1, 0);
+	    //pid.setOutputRange(-1, 0);
 	    zeroEncoder();
 	} else {
-	    pid.setOutputRange(-1, 1);
+	    //pid.setOutputRange(-1, 1);
 	}
     }
     
